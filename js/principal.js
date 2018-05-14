4eaba706b5ce47017059febf56b7c1cd86a2a33c
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-14 06:13:57 -0300

    Aula 04 Exercício 03

--- js/principal.js
+++ js/principal.js
@@ -36,3 +36,9 @@ for (var i = 0; i < pacientes.length; i++) {
 		tdImc.textContent = imc.toFixed(2);
 	}
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function() {
+	console.log("Oi, cliquei no botão");
+});
