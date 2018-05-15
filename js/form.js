50da5d29bfc4f679719aa614d49eb5f108f76ae4
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-15 06:38:33 -0300

    Indentando o código

--- js/form.js
+++ js/form.js
@@ -39,7 +39,7 @@ function montaTr(paciente) {
     pacienteTr.classList.add("paciente");
 
 	pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
-	pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
     pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
     pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
     pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
