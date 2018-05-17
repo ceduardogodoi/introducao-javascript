12e6ff34d2fdadcf770576bc253f2d9077685c3a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-16 22:33:41 -0300

    Aula 07 Exerício 06: Event bubbling, event.target, event.parentNode, setTimeOut, fadeOut no css

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,10 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick", function() {
-        console.log("Fui clicado com duplo clique");
-        this.remove();
-    })
+var tabela = document.querySelector("table");
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.classList.add("fadeOut");
+    
+    setTimeout(function () {
+        event.target.parentNode.remove();
+    }, 500);
 });
