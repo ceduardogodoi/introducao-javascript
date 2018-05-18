7cacbaba4a58ea5de95b125169456c0e48dcd588
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-18 07:04:24 -0300

    Aula 09 Exercício 05: Capturando erros da requisição com o atributo xhr.status (status==200 -> Ok, status==404 -> erro) e o atributo responseText que contém o texto da resposta

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -5,16 +5,28 @@ botaoBuscar.addEventListener("click", function(event) {
     
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
-    xhr.send();
+    xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes111");
 
     xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
+        if (xhr.status == 200) {
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+    
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.style.color = "red";
+            erroAjax.classList.remove("invisivel");
 
-        var pacientes = JSON.parse(resposta);
-
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            // Vídeo parou em 5:00 (Aula 09 Exercício 05)
+            // url contém erro proposital para simular o erro
+            // lembre-se de arruma-la
+        }
     });
+
+    xhr.send();
 });
